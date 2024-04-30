3b3ce1bf8ffd6b6efd5fe4fcc3317f17f87cb1e6
liogiladi <liogiladi@gmail.com>
2024-04-30 21:21:40 +0300

    remove useless connectStylesheet

--- utils/connectStylesheet.js
+++
@@ -1,8 +0,0 @@
-export function connectStylesheet(path) {
-	if (!document.head.querySelector(`link[href="${path}"]`)) {
-		const link = document.createElement("link");
-		link.rel = "stylesheet";
-		link.href = path;
-		document.head.append(link);
-	}
-}
