c1a3c9f3df6cc9572eee77dd2518661ec341f082
liogiladi <liogiladi@gmail.com>
2024-09-08 19:48:13 +0300

    update isMobile function to support more browsers (ie. firefox)

--- utils/isMobile.js
+++ utils/isMobile.js
@@ -3,22 +3,28 @@
  * @returns {boolean}
  */
 export default function isMobile() {
-    if ("maxTouchPoints" in navigator) {
-        return navigator.maxTouchPoints > 0;
-    } else if ("msMaxTouchPoints" in navigator) {
-        return navigator.msMaxTouchPoints > 0;
-    } else {
-        const mQ = matchMedia?.("(pointer:coarse)");
-        if (mQ?.media === "(pointer:coarse)") {
-            return !!mQ.matches;
-        } else if ("orientation" in window) {
-            return true; // deprecated, but good fallback
-        } else {
-            // Only as a last resort, fall back to user agent sniffing
-            return (
-                /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(navigator.userAgent) ||
-                /\b(Android|Windows Phone|iPad|iPod)\b/i.test(navigator.userAgent)
-            );
-        }
-    }
+	if ("ontouchstart" in window) {
+		if ("maxTouchPoints" in navigator) {
+			return navigator.maxTouchPoints > 0;
+		} else if ("msMaxTouchPoints" in navigator) {
+			return navigator.msMaxTouchPoints > 0;
+		}
+	} else {
+		const mQ = matchMedia?.("(pointer:coarse)");
+		if (mQ?.media === "(pointer:coarse)") {
+			return !!mQ.matches;
+		} else if ("orientation" in window) {
+			return true; // deprecated, but good fallback
+		} else {
+			// Only as a last resort, fall back to user agent sniffing
+			return (
+				/\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(
+					navigator.userAgent
+				) ||
+				/\b(Android|Windows Phone|iPad|iPod)\b/i.test(
+					navigator.userAgent
+				)
+			);
+		}
+	}
 }
